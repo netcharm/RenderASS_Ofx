8c810346cdd17c68e48fd6934a7469d425390dc9
netcharm <netcharm@163.com>
2018-01-18 03:17:56 +0800

    add CHT/JP I18N UI

--- RenderASS/mkpot.cmd
+++ RenderASS/mkpot.cmd
@@ -9,9 +9,27 @@ set BINDING=locale
 
 "%GETTEXT%" --package-name="%PACKAGE%" --package-version=1.0 --default-domain="%PACKAGE%" --c++ -k_ --from-code=utf-8 --output="%PACKAGE%.pot" *.cpp
 
-mkdir zh_CN\LC_MESSAGES
-rem "%MSGINIT%" --no-translator --locale zh_CN.UTF-8 --output-file "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po" --input "%PACKAGE%.pot"
+if exist "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po" (
+  %MSGMERGE% -U -N "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po" "%PACKAGE%.pot"
+  "%MSGFMT%" --check --verbose --output-file "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.mo" "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po"
+) else (
+  mkdir "%BINDING%\zh_CN\LC_MESSAGES"
+  "%MSGINIT%" --no-translator --locale zh_CN.UTF-8 --output-file "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po" --input "%PACKAGE%.pot"
+)
 
-%MSGMERGE% -U -N "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po" "%PACKAGE%.pot"
+if exist "%BINDING%\zh_TW\LC_MESSAGES\%PACKAGE%.po" (
+  %MSGMERGE% -U -N "%BINDING%\zh_TW\LC_MESSAGES\%PACKAGE%.po" "%PACKAGE%.pot"
+  "%MSGFMT%" --check --verbose --output-file "%BINDING%\zh_TW\LC_MESSAGES\%PACKAGE%.mo" "%BINDING%\zh_TW\LC_MESSAGES\%PACKAGE%.po"
+) else (
+  mkdir "%BINDING%\zh_TW\LC_MESSAGES"
+  "%MSGINIT%" --no-translator --locale zh_TW.UTF-8 --output-file "%BINDING%\zh_TW\LC_MESSAGES\%PACKAGE%.po" --input "%PACKAGE%.pot"
+)
 
-"%MSGFMT%" --check --verbose --output-file "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.mo" "%BINDING%\zh_CN\LC_MESSAGES\%PACKAGE%.po"
+if exist "%BINDING%\jp\LC_MESSAGES\%PACKAGE%.po" (
+  %MSGMERGE% -U -N "%BINDING%\jp\LC_MESSAGES\%PACKAGE%.po" "%PACKAGE%.pot"
+  "%MSGFMT%" --check --verbose --output-file "%BINDING%\jp\LC_MESSAGES\%PACKAGE%.mo" "%BINDING%\jp\LC_MESSAGES\%PACKAGE%.po"
+) else (
+  mkdir "%BINDING%\jp\LC_MESSAGES"
+  "%MSGINIT%" --no-translator --locale jp.UTF-8 --output-file "%BINDING%\jp\LC_MESSAGES\%PACKAGE%.po" --input "%PACKAGE%.pot"
+)
+
